2bcac06ae628e7fe086a82683835b7d950cbe5f0
Philip Pitha <ppitha@gmail.com>
2020-10-13 18:07:23 -0400

    removed comment

--- maybees/settings/base.py
+++ maybees/settings/base.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'tqg55leejwsx0+s%pwdt@!dz@fg(%k7r398mi)kn1g8@e7k7rj'
-
 def get_env_variable(var_name):
     try:
         return os.environ[var_name]
